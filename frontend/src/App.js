3c517975f33115db82c0546fd600e268805d0393
Saharsh Gupta <saharshg29@gmail.com>
2023-08-08 21:54:53 +0530

    addes login and signup components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
+import { Login } from './components/login';
+import Signup from './components/signup';
+// import { Nav } from './components/navigation';
+import { Route, Routes } from "react-router-dom"
 
 function App() {
   return (
     <div className="App">
-     Hello World
+      <Nav />
+      <Routes>
+        <Route path='/login' element={<Login />} />
+        <Route path='/signup' element={<Signup />} />
+      </Routes>
+
+
     </div>
   );
 }
